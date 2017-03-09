b17d2afb000f9330ff654025522f7aa20cf52b5c
yushen <215649498@qq.com>
2017-03-09 22:01:15 +1100

    Add a new file

--- src/leetcode/FizzBuzz.java
+++ src/leetcode/FizzBuzz.java
@@ -1,5 +1,32 @@
 package leetcode;
 
-public class FizzBuzz {
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class FizzBuzz {
+public List<String> fizzBuzz(int n) {
+	List<String> output = new ArrayList<>();
+	
+	for (int i = 0; i <n; i++) {
+		if ((i+1)%3==0&&(i+1)%5==0) {
+		output.add(i, "FizzBuzz");
+		}else if ((i+1)%3==0&&(i+1)%5!=0) {
+			output.add(i, "Fizz");
+		}else if ((i+1)%5==0&&(i+1)%3!=0) {
+			output.add(i, "Buzz");
+		}else 
+			output.add(String.valueOf(i+1));
+		
+		}
+	System.out.println(output);
+	return output;
+        
+    }
+
+public static void main(String args[]) {
+	FizzBuzz fb = new FizzBuzz();
+	fb.fizzBuzz(15);
+	
+}
 }
