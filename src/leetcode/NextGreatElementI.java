349592228fd80c8ed74140911776ccb869051758
yushen <215649498@qq.com>
2017-03-13 18:59:09 +1100

    fix

--- src/leetcode/NextGreatElementI.java
+++ src/leetcode/NextGreatElementI.java
@@ -1,6 +1,4 @@
 package leetcode;
-
-import java.io.LineNumberInputStream;
 import java.util.ArrayList;
 
 public class NextGreatElementI {
@@ -11,12 +9,12 @@ public int[] nextGreaterElement(int[] findNums, int[] nums) {
 	for(int i=0;i<findNums.length;i++){
 		line1.add(findNums[i]);
 	}
-	
+	System.out.println(line1);
 	ArrayList<Integer> line2=new ArrayList<>();
 	for(int j=0;j<nums.length;j++){
 		line2.add(nums[j]);
 	}
-	
+	System.out.println(line2);
 	for(int k=0;k<line1.size();k++){
 		int number=line1.get(k);
 		int number2 = line2.indexOf(number)+1;
@@ -24,8 +22,16 @@ public int[] nextGreaterElement(int[] findNums, int[] nums) {
 			output[k]=-1;
 		}else if (number2<line2.size()) {	
 			if (line2.get(number2)<number) {
-			output[k]=-1;
-		}else {
+				for (int l = number2; l < line2.size(); l++) {
+					if (line2.get(l)>number) {
+						 output[k]=line2.get(l);
+						 break;
+					}else {
+						output[k]=-1;
+					}
+				}
+			
+		}else if (line2.get(number2)>number) {
 			output[k]=line2.get(number2);
 		}
 	}
@@ -34,14 +40,20 @@ public int[] nextGreaterElement(int[] findNums, int[] nums) {
 	System.out.println(output[0]);
 	System.out.println(output[1]);
 	System.out.println(output[2]);
+	System.out.println(output[3]);
+	System.out.println(output[4]);
+	
 	return output;
         
     }
 
 public static void main(String args[]) {
 	NextGreatElementI ng=new NextGreatElementI();
-	int[] a={4,1,2};
-	int[] b={1,3,4,2};
+	int[] a={1,3,5,2,4};
+	int[] b={6,5,4,3,2,1,7};
+	
+	//int[] a={4,1,2};
+	//int[] b={1,3,4,2};
 	ng.nextGreaterElement(a, b);
 }
 
