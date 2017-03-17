dcd61755668fd98a0cf6e18c64ec01130c0da673
yushen <215649498@qq.com>
2017-03-17 18:29:18 +1100

    XOR

---
+++ src/leetcode/DetectCapital.java
@@ -0,0 +1,38 @@
+package leetcode;
+import java.util.ArrayList;
+
+public class DetectCapital {
+public boolean detectCapitalUse(String word) {
+	ArrayList<Integer> store = new ArrayList<>();
+	for(int i=0;i<word.length();i++){
+		if (word.charAt(i)>=97) {
+			store.add(1);
+		}else {
+			store.add(0);
+		}
+	}	
+	int outputC=0;
+	int outputL=0;
+	for (int i = 0; i < store.size(); i++) {
+		if (store.get(i)==0) {
+			outputC++;
+		}
+		else {
+			outputL++;
+		}
+	}	
+	if (store.get(0)==0 && outputC==store.size()) {return true;}
+	else if (store.get(0)==0 && outputL+1==store.size()) {return true;}
+	else if (store.get(0)==1 && outputL==store.size()) {return true;}
+	else 
+		return false;
+
+ }
+
+public static void main(String args[]) {
+	DetectCapital dc= new DetectCapital();
+	String word="usa";
+	System.out.println(dc.detectCapitalUse(word));
+	
+}
+}
--- src/leetcode/SingleNumber.java
+++ src/leetcode/SingleNumber.java
@@ -5,15 +5,13 @@ import java.util.ArrayList;
 public class SingleNumber {
 public int singleNumber(int[] nums) {
      int output=0;
-     //ArrayList<Integer> once=new ArrayList<>();
      for (int i = 0; i < nums.length; i++) {
 		output=output^nums[i];
-	}
-     
-    System.out.println(output);   
-    //output=once.get(0);
+	}     
+    System.out.println(output);      
     return output;
-    }
+    
+}
 public static void main(String args[]){
 	SingleNumber sn=new SingleNumber();
 	int[] nums={1};
